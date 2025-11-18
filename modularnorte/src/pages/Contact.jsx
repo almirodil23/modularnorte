7119be05e1a3889d36f4602e6ca6015c207f435f
Alvaro <almirodil@gmail.com>
2025-11-18 12:09:15 +0100

    18_11-12:09

--- modularnorte/src/pages/Contact.jsx
+++ modularnorte/src/pages/Contact.jsx
@@ -144,7 +144,6 @@ aa
               Responsable: Modular Norte (Construcciones Laminadas SL)...  
             </div>
 
-
           </form>
           <img src="/assets/custom/img/home.JPG" className="footer-img" />
         </div>
