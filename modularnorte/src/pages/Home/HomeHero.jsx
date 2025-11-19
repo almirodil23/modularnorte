86697183e67607e6b96c35f52d2703cb6705f9d9
Alvaro <almirodil@gmail.com>
2025-11-19 13:44:41 +0100

    final1911

--- modularnorte/src/pages/Home/HomeHero.jsx
+++ modularnorte/src/pages/Home/HomeHero.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export default function HomeHero() {
   return (
@@ -11,19 +12,21 @@ export default function HomeHero() {
         minHeight: "100vh",
       }}
     >
-        <div className="container-fluid">
+      <div className="container-fluid">
         <div className="row">
           <div className="col-12 textos">
             <div className="contenido">
               <h1 className="titulo">MODULAR NORTE</h1>
-              <h3 className="subtitulo">Arquitectura modular de diseño vanguardista</h3>
+              <h3 className="subtitulo">
+                Arquitectura modular de diseño vanguardista
+              </h3>
             </div>
           </div>
 
           <div className="col-12 boton">
-            <a href="/projects" className="boton_linea_blanco">
+            <Link to="/projects" className="boton_linea_blanco">
               Ver Proyectos
-            </a>
+            </Link>
           </div>
         </div>
       </div>
