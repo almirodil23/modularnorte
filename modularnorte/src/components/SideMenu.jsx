37d17afd89512a14f835f8246994ae681c9b6f85
Alvaro <almirodil@gmail.com>
2025-11-20 10:35:47 +0100

    20_11-10:35

--- modularnorte/src/components/SideMenu.jsx
+++ modularnorte/src/components/SideMenu.jsx
@@ -64,6 +64,7 @@ export default function SideMenu() {
           <li className="nav-item">
             <Link to="/contactar" className="nav-link" onClick={closeMenu}>Contacto</Link>
           </li>
+          
 
           <li className="redes">
             <a href="https://www.facebook.com/Modular-Projects-145924372121713" target="_blank">
