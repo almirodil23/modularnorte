fa72f6fdeb44bd04b6ab336eac6580c14ea4aebb
Álvaro <almirodil@gmail.com>
2025-11-26 13:19:59 +0100

    cambioform2

--- modularnorte/src/pages/Contact.jsx
+++ modularnorte/src/pages/Contact.jsx
@@ -50,7 +50,6 @@ export default function Contact() {
 
 
 
-
   return (
     <div className="pagina_con_fragmento_fijo container-fluid contact-page" style={{ backgroundColor: "white" }}>
       <div className="row justify-content-between">
