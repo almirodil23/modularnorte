8d8413d9ebd2247432124379a97f6b25f0d976d4
Álvaro <almirodil@gmail.com>
2026-09-10 09:54:00 +0200

    khg

--- modularnorte/src/main.jsx
+++ modularnorte/src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Clarity from '@microsoft/clarity';
 
 
 import App from './App.jsx'
+
+
+const projectId = "yg0l4plsf3"
+
+Clarity.init(projectId);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
